a884f96eb6e2bd18b69c377ea06f0c7488cfc903
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2023-09-10 22:06:28 +0100

    fix: Add board

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,3 +1,36 @@
 "use strict";
 
-function f() {}
+const gameBoard = (function () {
+  const _array = [];
+  for (let i = 0; i < 3; i++) {
+    const row = [];
+    for (let j = 0; j < 3; j++) {
+      row.push(null);
+    }
+    _array.push(row);
+  }
+
+  const htmlElementWrapper = (function () {
+    const htmlElement = document.createElement("div");
+    htmlElement.classList.add("game-board");
+    for (let i = 0; i < _array.length; i++) {
+      for (let j = 0; j < _array[i].length; j++) {
+        const cell = document.createElement("div");
+        htmlElement.appendChild(cell);
+        cell.classList.add("board-cell");
+        cell.dataset.x = j.toString();
+        cell.dataset.y = i.toString();
+      }
+    }
+
+    return { htmlElement };
+  })();
+
+  return { htmlElementWrapper };
+})();
+
+const main = (function () {
+  document.body.appendChild(gameBoard.htmlElementWrapper.htmlElement);
+
+  return {};
+})();
